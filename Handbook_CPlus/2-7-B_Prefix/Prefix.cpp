51e60bc67c3a38e8ca7a06dcf372516f5b9185fe
Waka2Bin <71177373+Waka2Bin@users.noreply.github.com>
2024-04-16 22:42:12 +0300

    2024-04-16 home

--- Handbook_CPlus/2-7-B_Prefix/Prefix.cpp
+++ Handbook_CPlus/2-7-B_Prefix/Prefix.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
 
 std::string CommonPrefix(const std::vector<std::string>& words);
 
 int main() {
 	using namespace std;
-	vector<string> word = {"application", "apple", "apricot"};
+	vector<string> word = { "","ssad","s" };
 	string answ {CommonPrefix(word)};
 	cout << answ;
 }
 
 std::string CommonPrefix(const std::vector<std::string>& words) {
-	std::sort(words.begin(), words.end());
-	std::string answ = words[0].substr(0, 1);
-	std::string val = answ;
-	for (int j = 1; j < int(words[0].length());++j) {
-		for (auto i : words) {
-			if (answ != i.substr(0, j)) return val;
+	if (!words.empty()) {
+		int temp = words[0].length();
+		if (temp == 0) return "";
+		std::string answ = words[0].substr(0, 1);
+		std::string val = answ;
+		int len = temp;
+		int* point = &len;
+		for (int j = 1; j < *point; ++j) {
+			for (auto i : words) {
+				temp = i.length();
+				if (len > temp) len = temp;
+				if (temp == 0) return "";
+				if (answ != i.substr(0, j)) return val;
+				if (int(answ.length()) == temp) return answ;
+			}
+			val = answ;
+			answ += words[0].substr(j, 1);
 		}
-		val = answ;
-		answ += words[0].substr(j, 1);
+		return answ;
 	}
-	return answ;
+	else return "";
 }
