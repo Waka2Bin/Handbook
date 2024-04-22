c1b616d1325760bab50404f4bb7708b6db5e00b3
Waka2Bin <eskalra@mail.ru>
2024-04-22 16:09:48 +0300

    2024.04.22 work

--- Handbook_CPlus/3-1-D_Redactor/Redactor.cpp
+++ Handbook_CPlus/3-1-D_Redactor/Redactor.cpp
@@ -6,42 +6,53 @@
 
 using namespace std;
 
-void Down(list<string>::iterator &val, const list<string> &dat);
-void Up(list<string>::iterator& val, const list<string>& dat);
-string CtrlX(const list<string>& s);
-string CtrlV(const list<string>& s);
+void Down(vector<string>::iterator& val, const vector<string> & data);
+void Up(vector<string>::iterator& val, const vector<string>& data);
+string CtrlX(vector<string>::iterator & val, vector<string>& data);
+string CtrlV(vector<string>::iterator & val, vector<string>& data, string& str);
 
 int main() {
 	ifstream in("input.txt");
 	ofstream out("output.txt");
-	string s,bufer;
-	list<string> data;
+	string s = "";
+	vector<string> data;
 	vector<string> comands;
 	while (!in.eof()) {
 		getline(in, s);
-		data.push_back(s);
-		if (s == "Down" || s == "Up" || s == "CtrlX" || s == "CtrlV")) comands.push_back(s);
+		if (s == "Down" || s == "Up" || s == "Ctrl+X" || s == "Ctrl+V") comands.push_back(s);
+		else if (!s.empty() && s!="") data.push_back(s);
 	}
+	s.clear();
 	auto it = data.begin();
-
-
-
-
-
+	for (auto& i : comands) {
+		if (i == "Down") Down(it, data);
+		else if (i == "Up") Up(it, data);
+		else if (i == "Ctrl+X") s = CtrlX(it, data);
+		else if (i == "Ctrl+V") s = CtrlV(it, data, s);
+		else {}
+	}
 	for (auto& i : data) {
 		out << i << "\n";
 	}
 }
 
-void Down(list<string>::iterator& val, const list<string>& dat) {
-	if (val != dat.cend()) ++val;
+void Down(vector<string>::iterator& val, const vector<string>& data) {
+	if (val != data.end()) ++val;
 }
-void Up(list<string>::iterator& val, const list<string>& dat) {
-	if (val != dat.cbegin()) --val;
+void Up(vector<string>::iterator& val, const vector<string>& data) {
+	if (val != data.begin()) --val;
 }
-string CtrlX(const list<string>& s) {
-
+string CtrlX(vector<string>::iterator& val, vector<string>& data) {
+	string temp = *val;
+	int it_t = distance(data.begin(), val);
+	data.erase(val);
+	val = data.begin() + it_t;
+	return temp;
 }
-string CtrlV(const list<string>& s) {
-
+string CtrlV(vector<string>::iterator& val, vector<string>& data, string& str) {
+	string temp = *val;
+	int it_t = distance(data.begin(), val);
+	data.insert(val, str);
+	val = data.begin() + it_t+1;
+	return temp;
 }
