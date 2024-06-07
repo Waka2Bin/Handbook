515e0d3f05dd578d7709600f069b88e5009ba900
Waka2Bin <eskalra@mail.ru>
2024-06-07 14:22:25 +0300

    2024.06.07 work_2

---
+++ Handbook_CPlus/3_4_C_PriorQueue/PriorQueue.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <string>
+
+int main() {
+	using namespace std;
+
+	queue<int>data;
+	string command;
+	while (cin>> command) {
+
+	}
+
+	return 0;
+}
+
