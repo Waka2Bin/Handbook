a92f241dfe8403c886580957d7a92981f1fca6fd
Waka2Bin <eskalra@mail.ru>
2024-06-13 15:42:25 +0300

    2024.06.13 work

--- Handbook_CPlus/3_4_C_PriorQueue/PriorQueue.cpp
+++ Handbook_CPlus/3_4_C_PriorQueue/PriorQueue.cpp
@@ -1,17 +1,39 @@
-#include <iostream>
 #include <queue>
 #include <vector>
 #include <string>
+#include <fstream>
 
 int main() {
 	using namespace std;
-
-	queue<int>data;
+	
+	ifstream in("input.txt");
+	ofstream out("output.txt");
+	priority_queue<int>data;
+	vector<string> answ;
 	string command;
-	while (cin>> command) {
 
+	while (getline(in, command)) {
+		string val = command.substr(0, 3);
+		
+		if ("CLE" == val) {
+			while (!data.empty())
+				data.pop();
+		}
+		else if ("ADD" == val) {
+			data.push(stoi(command.substr(3)));
+		}
+		else if ("EXT" == val) {
+			if (!data.empty()) {
+				answ.push_back(to_string(data.top()));
+				data.pop();
+			}
+			else answ.push_back("CANNOT");
+		}
+	}
+
+	for (auto f : answ) {
+		out << f << "\n";
 	}
 
 	return 0;
 }
-
