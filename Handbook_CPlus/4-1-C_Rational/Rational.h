fa0e376da0a81df98e473d5e26f320e56e91fc12
Waka2Bin <eskalra@mail.ru>
2024-06-28 17:12:25 +0300

    2024.06.08 work

--- Handbook_CPlus/4-1-C_Rational/Rational.h
+++ Handbook_CPlus/4-1-C_Rational/Rational.h
@@ -1,25 +1,89 @@
 #pragma once
 
+#include <numeric>
 
 class Rational {
 private:
-	int hight{ 0 };
-	int low{ 1 };
-	Rational(int hight, int low);
+	int hight = 0;
+	int low = 1;
+	
+	void Reduce() {
+		int d = std::gcd(hight, low);
+		hight /= d;
+		low /= d;
+		if (low < 0) {
+			hight *= -1;
+			low *= -1;
+		}
+	}
+
 public:
-	Rational& operator + () {
-
+	Rational() = default;// объявление дефолтного конструктора
+	Rational(int h, int l) : hight(h), low(l) {  // объявление конструктора
+		Reduce();
+	}; 
+	
+	Rational operator + () const {
+		return { hight, low };
 	}
-	Rational& operator - () {
-
+	Rational operator - () const {
+		return Rational{ -hight, low };
 	}
-	Rational& operator * () {
 
+	Rational& operator + (int val) { // операции с инт
+		hight += low * val;
+		return *this;
 	}
-	Rational& operator / () {
+	Rational& operator + (Rational& obj) { // перегрузка операции с классом
+		int h = hight;
+		int l = low;
+		hight = hight*obj.Denominator() + low*obj.Numerator();
+		low = low * obj.Denominator();
+		return *this;
+	}
+	
+	Rational& operator - (int val) {
+		hight -= low * val;
+		return *this;
+	}
+	Rational& operator - (Rational& obj) { 
+		int h = hight;
+		int l = low;
+		hight = hight * obj.Denominator() - low * obj.Numerator();
+		low = low * obj.Denominator();
+		return *this;
+	}
+	
+	Rational& operator * (int val) {
+		hight *= val;
+		return *this;
+	}
+	Rational& operator * (Rational& obj) {
+		int h = hight;
+		int l = low;
+		hight = h * obj.Numerator();
+		low = l * obj.Denominator();
+		return *this;
+	}
 
+	Rational& operator / (int val) {
+		low = low * val;
+		return *this;
 	}
-	int Numerator() const{
+	Rational& operator / (Rational& obj) {
+		int h = hight;
+		int l = low;
+		hight = h * obj.Denominator();
+		low = l * obj.Numerator();
+		return *this;
+	}
+
+	Rational& operator += (int val) {
+		hight -= low;
+	}
+
+
+	int Numerator() const{  //2 геттера
 		return hight;
 	}
 	int Denominator() const {
